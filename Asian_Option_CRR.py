4f026bf2ad18be13e2944b30afb05d3aba8eddc4
Michel Vanderhulst <michelvanderhuls@hotmail.com>
2023-11-17 12:30:16 +0100

    Added rebalancing at maturity

--- Asian_Option_CRR.py
+++ Asian_Option_CRR.py
@@ -30,13 +30,9 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
     ####################################################################################################################
     #####################                  START accounts initialization                           #####################
 
-    G_Stock, G_Intrinsic, G_Price, G_Portfolio, G_CashAccount, G_Shares = nx.Graph(), nx.Graph(), nx.Graph(), \
-                                                                          nx.Graph(), nx.Graph(), nx.Graph()
+    G_Stock, G_Intrinsic, G_Price, G_Portfolio, G_CashAccount, G_Shares = nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph(), nx.Graph()
 
-    stockprices, sumstockprices, optionintrinsicvalue, optionprice, labelStocks, labelsIntrinsic, labelsOptionPrice, \
-    CashAccount, NbrOfShares, Portfolio, labelPortfolio, labelCash, labelNbrOfShares, EquityAccount, labelEquity = {},\
-     {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
-
+    stockprices, sumstockprices, optionintrinsicvalue, optionprice, CashAccount, NbrOfShares, Portfolio = {}, {}, {}, {}, {}, {}, {}
 
     #####################                      END accounts initialization                         #####################
     ####################################################################################################################
@@ -94,22 +90,9 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
                 sumstockprices[(i + 1, j + counter)] = stockprices[(i, j)] * u + sumstockprices[(i, j)]
                 sumstockprices[(i + 1, j + 1 + counter)] = stockprices[(i, j)] * d + sumstockprices[(i, j)]
 
-                # labelStocks[(i, j)] = f"S = {round(stockprices[(i, j)],2)}, Y = {round(sumstockprices[(i, j)],2)}"
-                # labelStocks[(i + 1, j + counter)] = f"S = {round(stockprices[(i + 1, j + counter)],2)}, Y = {round(sumstockprices[(i + 1, j + counter)],2)}"
-                # labelStocks[(i + 1, j + 1 + counter)] = f"S= {round(stockprices[(i + 1, j + 1 + counter)],2)}, Y = {round(sumstockprices[(i + 1, j + 1 + counter)],2)}"
-
                 optionintrinsicvalue[(i, j)] =max(phi * (((1 / (i + 2)) * sumstockprices[(i, j)]) - K), 0)  
                 optionintrinsicvalue[(i + 1, j + counter)] = max(phi * (((1 / (i + 2)) * sumstockprices[(i + 1, j + counter)]) - K), 0)
                 optionintrinsicvalue[(i + 1, j + 1 + counter)] =  max(phi * (((1 / (i + 2)) * sumstockprices[(i + 1, j + 1 + counter)]) - K), 0)
-
-
-                # labelsIntrinsic[(i, j)] = round(optionintrinsicvalue[(i, j)], 1)
-                # labelsIntrinsic[(i + 1, j+counter)] = round(optionintrinsicvalue[(i + 1, j+counter)], 1)
-                # labelsIntrinsic[(i + 1, j + 1+counter)] = round(optionintrinsicvalue[(i + 1, j + 1+counter)], 1)
-
-                # labelsIntrinsic[(i, j)] = round(optionintrinsicvalue[(i, j)], 1)
-                # labelsIntrinsic[(i + 1, j+counter)] = round(optionintrinsicvalue[(i + 1, j+counter)], 1)
-                # labelsIntrinsic[(i + 1, j + 1+counter)] = round(optionintrinsicvalue[(i + 1, j + 1+counter)], 1)
 
                 counter += 1
 
@@ -121,36 +104,23 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
             # values at maturity are not discounted given we start there
             if i == tree__periods:
                 optionprice[(i, j)] = optionintrinsicvalue[(i, j)]
-                # labelsOptionPrice[(i, j)] = round(optionprice[(i, j)], 2)
 
             # all the others, until price at t=0
             else:
                 optionprice[(i, j)] = discFact * (probUp * optionprice[(i + 1, j + counter)] + probDown * optionprice[(i + 1, j + 1 + counter)])
-                # labelsOptionPrice[(i, j)] = round(optionprice[(i, j)], 2)
+
             counter += 1
 
     Portfolio[(0, 1)] = optionprice[(0, 1)]
-    # labelPortfolio[(0, 1)] = str(round(Portfolio[(0, 1)], 2))
 
     # changer l'algo qui tourne autour des nodes, car trop lent avec les nodes.
     for i in range(0, tree__periods + 1):
         counter = 0
         for j in range(1, (2**i) +1):
             if i < tree__periods:
-                # print(i, j)
                 # initialize / After Rebalancing
                 NbrOfShares[(i, j)] = (optionprice[(i + 1, j+counter)] - optionprice[(i + 1, j + 1+counter)]) / (  (u-d)*stockprices[(i,j)]  )
                 CashAccount[(i, j)] = Portfolio[(i, j)] - NbrOfShares[(i, j)] * stockprices[(i, j)]
-
-                # Labels for the graph
-                # if i == 0:
-                #     labelCash[(i, j)] = str(round(CashAccount[(i, j)], 2))
-                #     labelNbrOfShares[(i, j)] = str(round(NbrOfShares[(i, j)], 2))
-
-                # labelCash[(i, j)] += f",{round(CashAccount[(i, j)], 2)}"
-                # labelNbrOfShares[(i, j)] += f",{round(NbrOfShares[(i, j)], 2)}"
-                # labelPortfolio[(i, j)] += f",{round(CashAccount[(i, j)] + NbrOfShares[(i, j)] * stockprices[(i, j)], 2)}"
-
 
                 # Before Rebalancing
                 NbrOfShares[(i + 1, j+counter)] = NbrOfShares[(i, j)]
@@ -161,18 +131,11 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
                 CashAccount[(i + 1, j + 1+counter)] = CashAccount[(i, j)] * np.exp(rf * step)
                 Portfolio[(i + 1, j + 1+counter)] = CashAccount[(i + 1, j + 1+counter)] + NbrOfShares[(i + 1, j + 1+counter)] * stockprices[(i + 1, j + 1+counter)]
 
-                # Labels for the graph
-                # labelCash[(i + 1, j+counter)] = f"{round(CashAccount[(i + 1, j+counter)], 2)}"
-                # labelCash[(i + 1, j + 1+counter)] = f"{round(CashAccount[(i + 1, j + 1+counter)], 2)}"
-
-                # labelNbrOfShares[(i + 1, j+counter)] = f"{round(NbrOfShares[(i + 1, j+counter)], 2)}"
-                # labelNbrOfShares[(i + 1, j + 1+counter)] = f"{round(NbrOfShares[(i + 1, j + 1+counter)], 2)}"
-
-                # labelPortfolio[(i + 1, j+counter)] = f"{round(Portfolio[(i + 1, j+counter)], 2)}"
-                # labelPortfolio[(i + 1, j + 1 + counter)] = f"{round(Portfolio[(i + 1, j + 1 + counter)], 2)}"
-
                 counter += 1
 
+            elif i == tree__periods:
+                CashAccount[(i, j)] = CashAccount[(i, j)] + NbrOfShares[(i, j)] * stockprices[(i, j)]
+                NbrOfShares[(i, j)] = 0
 
     # creating the nodes itself in the networkx graph
     pos = {}
@@ -186,7 +149,6 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
     node_x = []
     node_y = []
     stocksLabel = []
-    intrinsicLabel = []
     optionpriceLabel = []
     portfolioLabel = []
     cashLabel = []
@@ -208,12 +170,6 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
         x, y = pos[node]
         node_x.append(x)
         node_y.append(y)
-        # stocksLabel.append(labelStocks[node])
-        # intrinsicLabel.append(labelsIntrinsic[node])
-        # optionpriceLabel.append(labelsOptionPrice[node])
-        # portfolioLabel.append(labelPortfolio[node])
-        # cashLabel.append(labelCash[node])
-        # nbrofsharesLabel.append(labelNbrOfShares[node])
 
         stocksLabel.append(round(stockprices[node],2))
         SumStockPricesLabel.append(round(sumstockprices[node],2))
@@ -221,6 +177,6 @@ def RepStrat_Asian_Option_CRR(CallOrPut, S, K, rf, T, mu, vol, tree__periods):
         portfolioLabel.append(round(Portfolio[node],2))
         cashLabel.append(round(CashAccount[node],2))
         nbrofsharesLabel.append(round(NbrOfShares[node],2))
-                                                                                                    #optionintrinsicvalue
+                                                                                                   
     return nbrofsharesLabel, cashLabel, portfolioLabel, optionpriceLabel, SumStockPricesLabel, stocksLabel, edge_x, edge_y, node_x, node_y, round(u,2), round(d,2), round(probUp,2), round(probDown,2)
 
